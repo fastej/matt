982b6ccaf85336039559a7ffb32e8982d4873db1
Eddie <eddiejoer@gmail.com>
2018-04-09 14:08:17 +0100

    added background images and content

--- matt.js
+++ matt.js
@@ -21,19 +21,12 @@ function updateHeight()
 	var r5 = $('.r5');
 	var ul = $('ul');
 	var conW = $('ul').width();
-	var line = $('.line');
-	var midline = $('#midline');
 	var docW = $(document).width();
 	var blocker = $('.blocker');
 	var wrap = $('.wrap');
-	var w1 = $('.w1');
-	var w2 = $('.w2');
-	var w3 = $('.w3');
-	var w4 = $('.w4');
-	var o1 = $('.o1');
-	var o2 = $('.o2');
 	var frosting = $('#frosting');
 	var overlay = $('.overlay');
+	var content = $('.content');
 	var b1 = $('#b1');
 	var b2 = $('#b2');
 	var b3 = $('#b3');
@@ -51,30 +44,17 @@ function updateHeight()
 	c3.css('margin-left', conW/2 - width/2 -1);
 	c4.css('margin-left', 2*conW/3 - width/2 -1);
 	c5.css('margin-left', 5*conW/6 - width/2);
-	midline.css('left',0.1*docW);
-	midline.css('top', conW/4 + 1.5);
-	line.css('width', conW/2);
-	midline.css('width', conW);
 	blocker.css('height', 2*width);
 	blocker.css('width', 2*width);
+	content.css('height', 2*conW/3);
+	content.css('width', 0.1*docW + conW/3);
 	wrap.css('height', conW/3);
 	wrap.css('width', conW/3);
 	wrap.css('left', -conW/6 + width/2);
 	wrap.css('top', -conW/6 + width/2);
-	w1.css('top', -conW/4 + width/2);
-	w1.css('left', -conW/4 + width/2);
-	w3.css('top', -conW/4);
-	w4.css('top', -conW/4 + width/2 + conW/Math.sqrt(32));
-	w4.css('left', -conW/4 + width/2 - conW/Math.sqrt(32));
 	frosting.css('height', 2*conW/3);
 	overlay.css('height', conW/3);
 	overlay.css('top', -conW/3);
-	//o4.css('top', conW/2);
-	//o3.css('top', -conW/3);
-	//o2.css('top', -conW/3);
-	//o1.css('top', -conW/4 - 2);
-	//w1.css('top',-conW/4);
-	//w1.css('left',conW/8);
 	b1.css('top', -width +1);
 	b1.css('left', 0.1*docW - width +1);
 	b2.css('top', -width + 1);
@@ -84,6 +64,36 @@ function updateHeight()
 	b4.css('left', 0.1*docW - width + 1);
 	b4.css('top', 2*conW/3 - width -1);
 }
+
+$('li').on('click', function(){
+	
+	var id = $(this).attr('data-id');
+	var conW = $('ul').width();
+	
+	$('.overlay',this).toggleClass('clicked');
+	$(this).mouseleave(function(){
+		$('.overlay',this).removeClass('clicked');
+	});
+	$('ul').css({'background-image':'url('+ id +'.jpg)'});
+	$('#frosting').css('opacity',0);
+	$('.cover').css('opacity', 1);
+	$('.c2.r2 > .wrap > .cover').delay(100).animate({opacity:0},400);
+	$('.c4.r2 > .wrap > .cover').delay(200).animate({opacity:0},430);
+	$('.c5.r3 > .wrap > .cover').delay(300).animate({opacity:0},460);
+	$('.c4.r4 > .wrap > .cover').delay(400).animate({opacity:0},500);
+	$('.c3.r3 > .wrap > .cover').delay(500).animate({opacity:0},550);
+	$('.c2.r4 > .wrap > .cover').delay(600).animate({opacity:0},620);
+	$('.c1.r3 > .wrap > .cover').delay(700).animate({opacity:0},700);
+});
+
+	$('li').on('click', function() {
+		var id = $(this).attr('data-id');
+		
+		$('.content > p').css('opacity', 0);
+		$('.content > p[data-id='+ id +']').animate({opacity:1},1000);
+		$('.r1 > .wrap > p').hide();
+		$('.r1 > .wrap > p[data-id='+ id +']').show();
+	});
 
 });
 
